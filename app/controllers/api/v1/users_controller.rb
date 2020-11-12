681e58541b16c4d7cdd627ec24f8defa1e6bbc41
wyounis25 <github email address>
2020-11-12 09:40:16 -0600

    preferences array

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::UsersController < ApplicationController
 
     private
     def user_params
-      params.require(:user).permit(:username,:password)
+      params.require(:user).permit(:username,:password,:preferences => [])
     end
     
 end
